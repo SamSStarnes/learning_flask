0fb725a7173dec560a5ae0efd7b0bb78c2338d41
Sam Starnes <samual-starnes@uiowa.edu>
2024-04-04 14:55:29 -0500

    Pink background complete

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "hello world, index"
+    return render_template("index.html")
+
 
 @app.route("/mike")
 def mike():
@@ -13,5 +16,8 @@ def mike():
     sum = n1 + n2
     return str(sum)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 4444)), debug=True
+    )
